e816200147c0a8cb2787289b264bae23f0d9e7a1
Amit1Sharma47 <amit1.sharma@netprophetsglobal.com>
2023-05-09 18:21:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import counterSlice, { decreament, increament, increamentByAmount } from "./redux/counterSlice";
+
+
 
 function App() {
+  const count =
+    useSelector(state => state.counter.value)
+  const dispatch = useDispatch();
+  const increamentHandler = () => {
+    dispatch(increament())
+  }
+  const decreamentHandler = () => {
+    dispatch(decreament())
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{count}</p>
+      <button onClick={increamentHandler}>+</button>
+      <button onClick={decreamentHandler}>-</button>
+      <button onClick={() => { dispatch(increamentByAmount(5)) }}>Increase by 5</button>
     </div>
   );
 }
